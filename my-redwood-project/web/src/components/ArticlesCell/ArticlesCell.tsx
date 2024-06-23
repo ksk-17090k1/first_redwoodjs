ce88c707df3085103081e556022d22f6e6e06bb4
inagaki-keisuke <kin71796@gmail.com>
2024-06-24 01:26:17 +0900

    update: params routeing

--- my-redwood-project/web/src/components/ArticlesCell/ArticlesCell.tsx
+++ my-redwood-project/web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -5,6 +5,7 @@ import type {
   CellFailureProps,
   TypedDocumentNode,
 } from '@redwoodjs/web'
+import { Link, routes } from '@redwoodjs/router'
 
 export const QUERY: TypedDocumentNode<
   ArticlesQuery,
@@ -34,7 +35,11 @@ export const Success = ({ articles }: CellSuccessProps<ArticlesQuery>) => {
       {articles.map((article) => (
         <article key={article.id}>
           <header>
-            <h2>{article.title}</h2>
+            <h2>
+              <Link to={routes.article({ id: article.id })}>
+                {article.title}
+              </Link>
+            </h2>
           </header>
           <p>{article.body}</p>
           <div>Posted at: {article.createdAt}</div>
